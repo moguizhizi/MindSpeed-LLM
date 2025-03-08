c01db44a8d2be052867589515d6bb5d45825f52b
MeiFei <meifei5@huawei.com>
2025-03-08 07:49:59 +0000

    !2359 shell bug fix and data_convert script for llama ppo Merge pull request !2359 from MeiFei/shell

---
+++ examples/mcore/llama32/data_convert_llama32_ppo.sh
@@ -0,0 +1,15 @@
+# 请按照您的真实环境修改 set_env.sh 路径
+source /usr/local/Ascend/ascend-toolkit/set_env.sh
+mkdir ./finetune_dataset
+
+python ./preprocess_data.py \
+    --input ./dataset/train-00000-of-00001-a09b74b3ef9c3b56.parquet \
+    --tokenizer-name-or-path ./model_from_hf/llama32-hf/ \
+    --output-prefix ./finetune_dataset/alpaca \
+    --workers 4 \
+    --log-interval 1000 \
+    --tokenizer-type PretrainedFromHF \
+    --overwrite-cache \
+    --handler-name PPOAlpacaStyleInstructionHandler \
+    --prompt-type llama3 \
+    # --map-keys '{"prompt":"instruction","query":"input","response":"output"}' # 默认值，可不传
