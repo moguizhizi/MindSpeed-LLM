7cbbd6232da3ae89c482ba1eeccb58b5abbb3b4b
yanzhixiao <yanzhixiao@h-partners.com>
2025-03-19 06:51:49 +0000

    !2413 bugfix:ulysses 显存劣化 Merge pull request !2413 from yanzhixiao/bugfix-ulysses

--- mindspeed_llm/tasks/megatron_adaptor.py
+++ mindspeed_llm/tasks/megatron_adaptor.py
@@ -249,7 +249,7 @@ class CoreAdaptation(MegatronAdaptationABC):
         from ..core import rotary_embedding_forward, apply_rotary_pos_emb_bshd
         from ..core.models.gpt.gpt_layer_specs import get_gpt_layer_local_spec_wrapper
         from ..core.transformer.dot_product_attention import dot_product_attention_init, \
-            dot_product_attention_forward_wrapper, ulysses_context_parallel_forward_wrapper
+            dot_product_attention_forward_wrapper, ulysses_context_parallel_forward
         from ..core.models.gpt.gpt_model import gpt_model_init_wrapper, shared_embedding_weight
         from ..core import rotary_embedding_init_wrapper, gpt_model_forward
 
@@ -305,7 +305,7 @@ class CoreAdaptation(MegatronAdaptationABC):
         # For GQA in ulysses and hybrid
         MegatronAdaptation.register(
             'mindspeed.core.context_parallel.ulysses_context_parallel.UlyssesContextAttention.forward',
-            ulysses_context_parallel_forward_wrapper)
+            ulysses_context_parallel_forward)
 
         # Layer Definition
         # For NPU, we use local-mcore-structrue in te layer.
