f6ef9fc6609d050cbf9c862cbb31e8bce6efb1c2
闻江 <wenjiang@huawei.com>
2025-03-20 03:35:11 +0000

    !2418 --use-fused-rotary-pos-emb在405B模型上多占用2G显存，改用--use-fused-rotary-pos-emb-new Merge pull request !2418 from 闻江/master

--- tests/poc/llama3_405b/pretrain_llama3_405B_8k_256die_A3.sh
+++ tests/poc/llama3_405b/pretrain_llama3_405B_8k_256die_A3.sh
@@ -68,8 +68,8 @@ GPT_ARGS="
     --hidden-dropout 0.0 \
     --position-embedding-type rope \
     --normalization RMSNorm \
+    --use-fused-rotary-pos-emb-new \
     --swiglu \
-    --use-fused-rotary-pos-emb \
     --use-fused-swiglu \
     --use-flash-attn \
     --use-fused-ring-attention-update \
